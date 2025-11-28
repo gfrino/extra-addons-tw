8fef9ba7b41f018ca6d56bd613810019fe7e3976
Giovanni Frino <executive@Mac-Studio-di-Giovanni-2238.local>
2025-11-28 14:35:47 +0100

    Update tw_delete_chatter_item to version 17.0.1.0.2 with updated images

--- tw_delete_chatter_item/__manifest__.py
+++ tw_delete_chatter_item/__manifest__.py
@@ -24,7 +24,7 @@
     "license": "LGPL-3",
     "author": "ticinoWEB",
     "website": "http://www.ticinoweb.tech",
-    "version": '17.0.1.0.1',
+    "version": '17.0.1.0.2',
     "depends": ['base', 'mail'],
     "images": [
         'static/description/banner.png',
